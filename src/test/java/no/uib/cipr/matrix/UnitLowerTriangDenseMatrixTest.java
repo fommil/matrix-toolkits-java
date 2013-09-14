1e51b90d4653733bd806cb8a7cee79828a5c8810
Sam Halliday <sam.halliday@gmail.com>
2013-09-14 20:37:43 +0100

    isolated a bug with LU matrices

--- src/test/java/no/uib/cipr/matrix/UnitLowerTriangDenseMatrixTest.java
+++ src/test/java/no/uib/cipr/matrix/UnitLowerTriangDenseMatrixTest.java
@@ -20,8 +20,6 @@
 
 package no.uib.cipr.matrix;
 
-import no.uib.cipr.matrix.UnitLowerTriangDenseMatrix;
-
 /**
  * Test of UnitLowerTriangDenseMatrix
  */
@@ -38,5 +36,27 @@ public class UnitLowerTriangDenseMatrixTest extends UnitTriangMatrixTestAbstract
         Ad = Utilities.unitLowerPopulate(A);
         Utilities.unitSet(Ad);
     }
+
+  public void testMultUpper() {
+    Matrix lu = new DenseMatrix(new double[][]{
+        {-4.00,  6.00, 3.00},
+        {1.00, -8.00,  5.00},
+        {-0.50, -0.25,  0.75}
+    });
+    Matrix l = new UnitLowerTriangDenseMatrix(lu, false);
+    Matrix u = new UpperTriangDenseMatrix(lu, false);
+
+    System.out.println(l.mult(u, new DenseMatrix(3,3)));
+    System.out.println(l.mult(new DenseMatrix(u), new DenseMatrix(3,3)));
+
+    Matrix e = new DenseMatrix(new double[][]{
+        {-4,    6,    3},
+        {-4,   -2,    8},
+        {2,   -1,   -2}
+    });
+
+    Matrix out = l.mult(u, new DenseMatrix(3,3));
+    MatrixTestAbstract.assertEquals(e, out);
+  }
 
 }
